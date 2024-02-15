e878d85541a5dc8f9235a826d8efc307c0a20d58
webwarrior <reg@webwarrior.ws>
2024-02-15 13:30:24 +0100

    Add project files.

---
+++ Imports.cs
@@ -0,0 +1,5 @@
+global using SampleMauiAppCPM;
+global using SampleMauiAppCPM.Views;
+
+// Static
+global using static Microsoft.Maui.Graphics.Colors;
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SampleMauiAppCPM
+{
+    [Register(nameof(AppDelegate))]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
